3291c4c628483de114b6afd1caf7b2490ccb26b1
kaluogh <zhangwenxin6615006@163.com>
2020-06-08 21:54:03 +0900

    Update SysUserServiceImpl.java

--- tomato-admin/src/main/java/com/dzm/tomatoadmin/serviceImpl/SysUserServiceImpl.java
+++ tomato-admin/src/main/java/com/dzm/tomatoadmin/serviceImpl/SysUserServiceImpl.java
@@ -3,11 +3,13 @@ package com.dzm.tomatoadmin.serviceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.dzm.tomatoadmin.dao.SysUserMapper;
 import com.dzm.tomatoadmin.model.SysUser;
 import com.dzm.tomatoadmin.service.SysUserService;
 
+@Service
 public class SysUserServiceImpl implements SysUserService{
 	
 	@Autowired
