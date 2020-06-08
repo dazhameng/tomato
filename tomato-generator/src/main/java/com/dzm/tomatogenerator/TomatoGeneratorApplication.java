b3ef72f087b3c475b282a43443246e80e68cda49
kaluogh <zhangwenxin6615006@163.com>
2020-06-08 17:44:56 +0900

    add tomato-generator

---
+++ tomato-generator/src/main/java/com/dzm/tomatogenerator/TomatoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.dzm.tomatogenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoGeneratorApplication.class, args);
+	}
+
+}
---
+++ tomato-generator/src/test/java/com/dzm/tomatogenerator/TomatoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzm.tomatogenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatoGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
