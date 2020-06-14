2885e4e9c381ba60fb7635a04e18b9d2ca455dbd
kaluogh <zhangwenxin6615006@163.com>
2020-06-14 17:14:19 +0900

    Create IoUtil.java

---
+++ tomato/tomato-common/src/main/java/com.dzm.tomato.common/utils/IoUtil.java
@@ -0,0 +1,25 @@
+package com.dzm.tomato.common.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * IoUtil
+ */
+
+public class IoUtil {
+
+    /**
+     * execute close
+     * @param closeable
+     */
+    public static void closeQuietly(final Closeable closeable){
+        try{
+            if (closeable != null){
+                closeable.close();
+            }
+        }catch (final IOException ioException){
+            // ignore
+        }
+    }
+}
